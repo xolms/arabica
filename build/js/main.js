179442d81d94b4209bc2fbee7331a3cd240cb4fd
xolms <xolms111@ya.ru>
2017-09-09 18:03:46 +0300

    all

---
+++ build/js/main.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$(".date").datepicker()});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOlsiJCIsImRvY3VtZW50IiwicmVhZHkiLCJkYXRlcGlja2VyIl0sIm1hcHBpbmdzIjoiQUFBQUEsRUFBRUMsVUFBVUMsTUFBTSxXQUNkRixFQUFFLFNBQVNHIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKGRvY3VtZW50KS5yZWFkeShmdW5jdGlvbiAoKSB7XHJcbiAgICAkKCcuZGF0ZScpLmRhdGVwaWNrZXIoKTtcclxufSk7XHJcbiJdfQ==
